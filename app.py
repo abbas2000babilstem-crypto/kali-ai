465fce1a0681672f61a56b49c632e7eb359fe885
abbas2000babilstem-crypto <abbas2000babilstem@gmail.com>
2026-05-28 19:49:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ system_instruction = """
 
 # إنشاء الموديل
 model = genai.GenerativeModel(
-    model_name="models/gemini-1.5-flash",
+    model_name="gemini-1.5-flash-latest",
     system_instruction=system_instruction
 )
 
